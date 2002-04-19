4277ac1709a31c2b26cb7de575425241bc583ee7
Jeroen Janssen <japj@xs4all.nl>
2002-04-19 17:38:29 +0000

    - fixing preprocessing of vgabios with latest gcc (from Mandrake 8.2)

--- vbe.c
+++ vbe.c
@@ -64,7 +64,7 @@ extern char vbebios_product_revision;
 extern Bit16u vbebios_mode_list;
 #endif
 
-#asm
+##asm
 // FIXME: 'merge' these (c) etc strings with the vgabios.c strings?
 _vbebios_copyright:
 .ascii       "Bochs/Plex86 VBE(C) 2002 Jeroen Janssen <japj@darius.demon.nl>"
@@ -120,12 +120,12 @@ _vbebios_mode_list:
 .word VBE_VESA_MODE_END_OF_LIST
 #endif
 
-#endasm
+##endasm
 
 // from rombios.c
 #define PANIC_PORT 0x501
 
-#asm
+##asm
 MACRO HALT
   ;; the HALT macro is called with the line number of the HALT call.
   ;; The line number is then sent to the PANIC_PORT, causing Bochs to
@@ -136,7 +136,7 @@ MACRO HALT
   mov ax,#?1
   out dx,ax 
 MEND
-#endasm
+##endasm
 
 // DISPI ioport functions
 // FIXME: what if no VBE host side code?
@@ -233,9 +233,9 @@ void vbe_init()
   if (dispi_id!=VBE_DISPI_ID0)
   {
 //FIXME this results in a 'rombios.c' line panic, but it's actually a 'vbe.c' panic
-#asm    
+##asm    
     HALT(__LINE__)
-#endasm    
+##endasm    
   }
 //#ifdef DEBUG
   printf("VBE Bios $Id$\n");
@@ -249,21 +249,21 @@ void vbe_display_info()
   // Check for VBE display extension in Bochs
   if (vbe_has_vbe_display())
   {
-    #asm
+    ##asm
      mov ax,#0xc000
      mov ds,ax
      mov si,#_vbebios_info_string
      call _display_string
-    #endasm  
+    ##endasm  
   }
   else
   {
-    #asm
+    ##asm
      mov ax,#0xc000
      mov ds,ax
      mov si,#_no_vbebios_info_string
      call _display_string
-    #endasm  
+    ##endasm  
     
   }
 }  

3f4f870418c6cb261c6e93186420e02601d6ef66
Christophe Bothamy <cbothamy@users.sourceforge.net>
2003-08-15 23:49:27 +0000

    - update the Makefile for releases - remove references to old plex86 website - update the Makefile so it build VGABIOS-lgpl-latest.bin and   VGABIOS-lgpl-latest.debug.bin

--- vgabios.c
+++ vgabios.c
@@ -186,7 +186,11 @@ vgabios_name:
 .byte   0x00
 
 vgabios_version:
+#ifndef VGABIOS_VERS
 .ascii	"current-cvs"
+#else
+.ascii VGABIOS_VERS
+#endif
 .ascii	" "
 
 vgabios_date:
@@ -208,8 +212,8 @@ vgabios_license:
 vgabios_website:
 .ascii	"Please visit :"
 .byte	0x0a,0x0d
-.ascii  " . http://www.plex86.org"
-.byte	0x0a,0x0d
+;;.ascii  " . http://www.plex86.org"
+;;.byte	0x0a,0x0d
 .ascii	" . http://bochs.sourceforge.net"
 .byte	0x0a,0x0d
 .ascii	" . http://www.nongnu.org/vgabios"
